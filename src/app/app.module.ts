ffe5552f525c75d479df0a5fc4f70427aa3f0316
Dominik Kubis <kubis.dominik1@gmail.com>
2017-06-06 20:01:29 +0200

    Create basic Playlist view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { PlaylistComponent } from './playlist/playlist.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
+import { ContentCardComponent } from './playlists/content-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlaylistComponent
+    PlaylistsComponent,
+    ContentCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
